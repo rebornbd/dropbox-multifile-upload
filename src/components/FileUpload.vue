2a91153d30e7346944999914150906f019d76e9d
dipta <me.dipta.dhar@gmail.com>
2022-11-23 21:17:57 +0600

    add tailwind

--- src/components/FileUpload.vue
+++ src/components/FileUpload.vue
@@ -1,12 +1,34 @@
-<script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true
-  }
-})
-</script>
-
 <template>
-  <h1>FILE UPLOAD</h1>
+  <div>
+    <h1>FILE UPLOAD</h1>
+    <icon name="upload" />
+  </div>
 </template>
+
+<script>
+  import Icon from './icons/IconFileUpload.vue';
+
+  export default {
+    data() {
+      return {
+        listOfFiles: []
+      }
+    },
+    methods: {
+      onchangeHandler(target, files) {
+        const filesLength = files.length;
+        for (let i=0; i<filesLength; i++) {
+          const data = {};
+          data.name = files[i]?.name;
+          data.size = files[i]?.size;
+          data.type = files[i]?.type;
+          this.listOfFiles.push(data);
+        }
+        target.value = null;
+      }
+    },
+    components: {
+      Icon,
+    }
+  }
+</script>
--- src/components/icons/IconUpload.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <svg class="ov-icon" aria-hidden="true" width="57.599999999999994" height="57.599999999999994" viewBox="0 0 32 32" fill="#222F3D" style="font-size: 3.6em;">
-  <path d="M16 3.594l-.719.687-7 7L9.72 12.72 15 7.438V24h2V7.437l5.281 5.282 1.438-1.438-7-7zM7 26v2h18v-2z"></path>
-</svg>
-</template>
